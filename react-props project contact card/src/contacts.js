26618f6528d3d8a88f97b22e21991f564c90eda4
satyamagarwal99 <56651086+satyamagarwal99@users.noreply.github.com>
2021-09-19 15:41:23 +0530

    Add files via upload

---
+++ react-props project contact card/src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "ShahRukh ",
+    imgURL:
+      "https://th.bing.com/th/id/OIP.KTqqG-lEh2auMwbr6tmynAHaFj?pid=ImgDet&rs=1",
+    phone: "+91 328736989",
+    email: "shahrukh@kham.com"
+  },
+  {
+    name: "Kiara Advani",
+    imgURL:
+      "https://upload.wikimedia.org/wikipedia/commons/6/67/Kiara_Advani_walks_for_Shyamal-Bhumika_at_India_Couture_Week_2018_Day_4_%2803%29_%28cropped%29.jpg",
+    phone: "+91 7654321",
+    email: "kiara@advani.com"
+  },
+  {
+    name: "Ritesh Agarwal",
+    imgURL: "https://wallpapercave.com/wp/wp7431525.jpg",
+    phone: "+91 8372574",
+    email: "ritesh@oyorooms.com"
+  }
+];
+
+export default contacts;
